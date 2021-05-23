780614dee102e910061620cec2f3639b78ea6275
Leonardo Murça <leonardoamurca@gmail.com>
2021-05-23 00:42:13 -0300

    Add body parser and cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const routes = require("./routes");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("./database");
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:8081" }));
+app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(routes);
