7d57760012a90ba832c34d1df9baca61fca75498
Leonardo Murça <leonardoamurca@gmail.com>
2021-05-23 15:53:21 -0300

    Update env files

--- src/config/database.js
+++ src/config/database.js
@@ -14,10 +14,10 @@ module.exports = {
   },
   production: {
     dialect: "postgres",
-    host: process.env.POSTGRES_HOST,
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASS,
-    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST_PROD,
+    username: process.env.POSTGRES_USERNAME_PROD,
+    password: process.env.POSTGRES_PASS_PROD,
+    database: process.env.POSTGRES_DB_PROD,
     port: process.env.POSTGRES_PORT,
     define: {
       timestamps: true,
